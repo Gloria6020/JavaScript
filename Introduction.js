219ac6bd0f49d78d12d90e9ecc17988d5aac1d78
Gloria6020 <129518115+Gloria6020@users.noreply.github.com>
2023-05-01 23:55:26 +0300

    Day1

---
+++ Introduction.js
@@ -0,0 +1,15 @@
+// console.log you can either use double quotes or single quotes
+console.log("This is my first javascript code");
+
+//console.log with multiple Arguments which takes multiple parameters seperated by commas
+console.log("gloria", "pkiach","mary");
+
+//  Arithmetics;Addition, Subtraction, multiplication, division, Modulus-finding remainder, exponentiation
+console.log(2 + 3)
+console.log(3 - 2)
+console.log(2 * 3)
+console.log(3 / 2)
+console.log(3 % 2)
+console.log(3 ** 2)//3 ** 2 == 3 ** 3
+
+
---
+++ variables.js
@@ -0,0 +1,9 @@
+// Declaring different variables of different data types
+let firstName = 'gloria' // first name of a person
+let lastName = 'cherop' // last name of a person
+let country = 'kenya' // country
+let city = 'Nairobi' // capital city
+let age = 21 // age in years
+let isMarried = false
+
+console.log(firstName, lastName, country, city, age, isMarried)
