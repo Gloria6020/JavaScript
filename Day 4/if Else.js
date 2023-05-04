bdf22377da8a8f1ad07f9a1439bfd11bd7dfdd48
Gloria6020 <129518115+Gloria6020@users.noreply.github.com>
2023-05-04 17:47:07 +0300

    conditionals

---
+++ Day 4/if Else.js
@@ -0,0 +1,33 @@
+//If condition is true the first block will be executed, if not the else condition will be executed.
+let num = 4
+if ( num > 1 ) {
+    console.log (`${num} is a positive number`)
+} else {
+    console.log (`${num} is a positive number`)
+}
+
+
+
+let cat = 2
+if ( cat < 1)  {
+    console.log  (`${num} is a positive number`)
+}else  {
+    console.log  (`${num} is a bad`)
+}
+
+// lodical
+let elvis = true
+if(elvis)  {
+    console.log(`he is my brother`)
+} else {
+    console.log(`he is not my brother`)
+}
+
+
+isRaining = false
+if (isRaining) {
+  console.log('You need a rain coat.')
+} else {
+  console.log('No need for a rain coat.')
+}
+// No need for a rain coat.
---
+++ Day 4/if.js
@@ -0,0 +1,19 @@
+/*In JavaScript and other programming languages the key word if is to used check if a condition is
+ true and to execute the block code.
+ To create an if condition, 
+we need if keyword, condition inside a parenthesis and block of code inside a curly bracket({}). */
+
+
+
+let num = 3
+if (num > 0) {
+  console.log(`${num} is a positive number`)
+}
+//  3 is a positive number
+
+
+//if the condition is false
+let isRaining = true
+if (isRaining!=true) {
+  console.log('Remember to take your rain coat.')
+}
