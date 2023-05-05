653e38bd76540cf7085feec17e4944ee80f4caef
Gloria6020 <129518115+Gloria6020@users.noreply.github.com>
2023-05-05 23:32:06 +0300

    arrays

---
+++ Day 5/arrays with values.js
@@ -0,0 +1,8 @@
+const numbers = [0, 3.14, 9.81, 37, 98.6, 100] // array of numbers
+console.log ('numbers:', numbers)
+console.log ( 'number of numbers:' , numbers.length)
+
+
+const fruits = ['banana', 'orange', 'mango', 'lemon'] // array of strings, fruits
+console.log ('fruits:', fruits)
+console.log ('number of fruits:', fruits.length)
