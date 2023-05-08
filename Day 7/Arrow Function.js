8db4203fd42026b21d8a10b48c4a98d6cf891075
Gloria6020 <129518115+Gloria6020@users.noreply.github.com>
2023-05-08 21:52:15 +0300

    Functions

---
+++ Day 7/Arrow Function.js
@@ -0,0 +1,13 @@
+//Arrow function uses arrow instead of the keyword function to declare a function.
+const changeToUpperCase = arr => {
+    const newArr = []
+    for (const element of arr) {
+      newArr.push(element.toUpperCase())
+    }
+    return newArr
+  }
+  
+  const countries = ['Finland', 'Sweden', 'Norway', 'Denmark', 'Iceland']
+  console.log(changeToUpperCase(countries))
+  
+  // ["FINLAND", "SWEDEN", "NORWAY", "DENMARK", "ICELAND"]
