f17f6e772d03ec578589ae188d02eb80e5aeb044
Gloria6020 <129518115+Gloria6020@users.noreply.github.com>
2023-05-09 21:01:59 +0300

    Higher order function

---
+++ Day 9/callback.js
@@ -0,0 +1,14 @@
+//A callback is a function which can be passed as parameter to other function
+// a callback function, the name of the function could be any name
+const numbers = [1, 2, 3, 4, 5]
+const sumArray = arr => {
+  let sum = 0
+  const callback = function(element) {
+    sum += element
+  }
+  arr.forEach(callback)
+  return sum
+
+}
+console.log(sumArray(numbers))
+
