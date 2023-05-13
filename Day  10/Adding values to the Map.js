d31ab168815099a8122c7ad895483bcec9d6d16f
Gloria6020 <129518115+Gloria6020@users.noreply.github.com>
2023-05-13 13:12:09 +0300

    Map

---
+++ Day  10/Adding values to the Map.js
@@ -0,0 +1,7 @@
+const countriesMap = new Map()
+console.log(countriesMap.size) // 0
+countriesMap.set('Finland', 'Helsinki')
+countriesMap.set('Sweden', 'Stockholm')
+countriesMap.set('Norway', 'Oslo')
+console.log(countriesMap)
+console.log(countriesMap.size)
---
+++ Day  10/Getting a value from Map.js
@@ -0,0 +1,8 @@
+const countriesMap = new Map()
+console.log(countriesMap.size) // 0
+countriesMap.set('Finland', 'Helsinki')
+countriesMap.set('Sweden', 'Stockholm')
+countriesMap.set('Norway', 'Oslo')
+console.log(countriesMap.get('Finland'))
+//Checking key in Map
+console.log(countriesMap.has('Finland'))
